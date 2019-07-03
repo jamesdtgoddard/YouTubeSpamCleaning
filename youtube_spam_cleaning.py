c748385860c215474809e8f0fbe53ee15bcc81f8
jamesdtgoddard <jamesdtgoddard@gmail.com>
2019-07-03 11:11:05 +0100

    Cleaned up code

--- youtube_spam_cleaning.py
+++ youtube_spam_cleaning.py
@@ -5,97 +5,159 @@ import numpy as np
 from random import random
 import glob
 
-#Import all datasets into df variable
-all_files = glob.glob("/Users/relativeinsight/Desktop/Youtube Spam Cleaning"+"/*.csv")
-li = []
-for filename in all_files:
-	dftemp = pd.read_csv(filename)
-	li.append(dftemp)
-df = pd.concat(li, axis=0, ignore_index=True)
+#Set default parameters
+FILE_PATH = "/Users/relativeinsight/Desktop/Youtube Spam Cleaning"
+FEATURES_LIST = ['LENGTH', 'SYMBOLS', 'CAPITALS']
+PROPORTION_TESTING = 0.25
 
-#Column for the string length of each comment
-df['LENGTH'] = df['CONTENT'].str.len()
+#Load all csv files into dataframe 'df'
+def importData(file_path):
 
-#Column for the number of capital letters in each comment
-capitals = []
-for i in df['CONTENT']:
-    sum = 0
-    for j in str(i):
-        if(j.isupper()):
-            sum += 1
-    capitals.append(sum)
-df['CAPITALS'] = capitals
+	global df
 
-#Column for the number of symbols (characters other than letters, numbers or spaces) in each comment
-symbols = []
-for i in df['CONTENT']:
-	sum = 0
-	for j in i:
-		if(not j.isalpha() and not j.isdigit() and j != ' '):
-			sum += 1
-	symbols.append(sum)
-df['SYMBOLS'] = symbols
+	all_files = glob.glob(file_path+"/*.csv")
+	temporary_list = []
 
-#Create list called features that only contains the columns that are to be used as feautures in the model
-features = []
-iteration = 0
-for i in df['CONTENT']:
-	features.append([df['LENGTH'][iteration], df['CAPITALS'][iteration], df['SYMBOLS'][iteration]])
-	iteration += 1
-features = np.array(features)
+	for filename in all_files:
+		temporary_df = pd.read_csv(filename)
+		temporary_list.append(temporary_df)
 
-#Create numpy array labels holding the true class of all comments
-labels = np.array(df['CLASS'])
+	df = pd.concat(temporary_list, axis=0, ignore_index=True)
 
-#Split dataset 75/25, train random forest model and make predictions on test data
-train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size = 0.25, random_state = 42)
-model = RandomForestClassifier(n_estimators = 1000)
-model.fit(train_features, train_labels);
-predictions_float = model.predict(test_features)
+#Create a list 'features' that contains all the extracted features listed in 'features_list'
+def extractFeatures(features_list):
 
-#Create list storing the absolute predictions made by the model
-predictions = []
-for i in predictions_float:
-	if(i > 0.5):
-		predictions.append(1)
-	else:
-		predictions.append(0)
+	global features
 
-#Create list storing the actual class values of all the comments
-actual_values = []
-for i in test_labels:
-	actual_values.append(i)
+	if('LENGTH' in features_list):
+		df['LENGTH'] = df['CONTENT'].str.len()
 
-#Randomly guess the class and compare with predictions to see how good the model is
-random_guesses = []
-random_number = 0
-random_correct = 0
-random_wrong = 0
-for i in range(0, len(actual_values)):
-	random_number = random()
-	if(random_number > 0.5):
-		if(actual_values[i] == 1):
-			random_correct += 1
+	if('CAPITALS' in features_list):
+		capitals = []
+		if('LENGTH' in features_list):
+			for i in range(0, len(df['CONTENT'])):
+				sum = 0
+				for j in df['CONTENT'][i]:
+					if(j.isupper()):
+						sum += 1
+				capitals.append(float(sum / df['LENGTH'][i]))
 		else:
-			random_wrong += 1
-	else:
-		if(actual_values[i] == 0):
-			random_correct += 1
+			for i in df['CONTENT']:
+				sum = 0
+				for j in i:
+					if(j.isupper()):
+						sum += 1
+				capitals.append(sum)
+		df['CAPITALS'] = capitals
+
+	if('SYMBOLS' in features_list):
+		symbols = []
+		if('LENGTH' in features_list):
+			for i in range(0, len(df['CONTENT'])):
+				sum = 0
+				for j in df['CONTENT'][i]:
+					if(not j.isalpha() and not j.isdigit() and j != ' '):
+						sum += 1
+				symbols.append(float(sum / df['LENGTH'][i]))
 		else:
-			random_wrong += 1
-random_ratio = float(random_correct / (random_correct + random_wrong))
+			for i in df['CONTENT']:
+				sum = 0
+				for j in i:
+					if(j.isupper()):
+						sum += 1
+				symbols.append(sum)
+		df['SYMBOLS'] = symbols
 
-#Calculate ratio actually predicted correctly by model
-predictions_correct = 0
-predictions_wrong = 0
-for i in range(0, len(actual_values)):
-	if(predictions[i] == actual_values[i]):
-		predictions_correct += 1
-	else:
-		predictions_wrong += 1
-predictions_ratio = float(predictions_correct / (predictions_correct + predictions_wrong))
+	features = []
+	individual_features = []
+	for i in range(0, len(df['CONTENT'])):
+		individual_features = []
+		if('LENGTH' in features_list):
+			individual_features.append(df['LENGTH'][i])
+		if('CAPITALS' in features_list):
+			individual_features.append(df['CAPITALS'][i])
+		if('SYMBOLS' in features_list):
+			individual_features.append(df['SYMBOLS'][i])
+		features.append(individual_features)
+	features = np.array(features)
 
-#Calculate difference in percentage of classes predicted correctly and those guessed randomly
-improvement = (predictions_ratio - random_ratio) * 100
-print("Model improvement: "+str(improvement),"("+str(predictions_ratio*100)+", "+str(random_ratio*100)+")")
+#Create labels list and split data for training and testing
+def splitData(proportion_testing):
 
+	global train_features, test_features, train_labels, test_labels
+
+	labels = np.array(df['CLASS'])
+
+	train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size = proportion_testing)
+
+
+#Train model and create predictions on test data
+def runModel():
+
+	global predictions, actual_values, predictions_ratio
+
+	model = RandomForestClassifier(n_estimators = 100)
+	model.fit(train_features, train_labels);
+	predictions_float = model.predict(test_features)
+
+	predictions = []
+	for i in predictions_float:
+		if(i > 0.5):
+			predictions.append(1)
+		else:
+			predictions.append(0)
+
+	actual_values = []
+	for i in test_labels:
+		actual_values.append(i) #Maybe remove loop
+
+	predictions_correct = 0
+	predictions_wrong = 0
+	for i in range(0, len(actual_values)):
+		if(predictions[i] == actual_values[i]):
+			predictions_correct += 1
+		else:
+			predictions_wrong += 1
+	predictions_ratio = float(predictions_correct / (predictions_correct + predictions_wrong))
+
+
+#Guess 1 or 0 to get an idea of the proportion we would expect to get right randomly
+def runRandom():
+
+	global random_guesses, random_ratio
+
+	random_guesses = []
+	random_number = 0
+	random_correct = 0
+	random_wrong = 0
+
+	for i in range(0, len(actual_values)):
+		random_number = random()
+		if(random_number > 0.5):
+			if(actual_values[i] == 1):
+				random_correct += 1
+			else:
+				random_wrong += 1
+		else:
+			if(actual_values[i] == 0):
+				random_correct += 1
+			else:
+				random_wrong += 1
+	random_ratio = float(random_correct / (random_correct + random_wrong))
+
+#Check difference in proportion predicted right and proportion guessed right
+def checkImprovement():
+
+	global improvement
+
+	improvement = (predictions_ratio - random_ratio) * 100
+
+#Run all functions
+def main(file_path, features_list, proportion_testing):
+	importData(file_path)
+	extractFeatures(features_list)
+	splitData(proportion_testing)
+	runModel()
+	runRandom()
+	checkImprovement()
+	return improvement
